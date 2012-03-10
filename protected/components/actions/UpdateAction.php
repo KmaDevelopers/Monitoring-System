050cde2e02f79e6b1e4ae99ab26a157cd894506a
Artem Rossokha <artem@asus.(none)>
2012-03-10 13:10:19 +0200

    save

--- protected/components/actions/UpdateAction.php
+++ protected/components/actions/UpdateAction.php
@@ -7,24 +7,44 @@ class UpdateAction extends CAction {
 
 		$json = file_get_contents('php://input');
 		$data = CJSON::decode($json);
-		$id = $data[$name.'Id'];
 
 		if ($id) {
-			$model = KmaActiveRecord::model(ucfirst($name))->findByPk($id);
+			$this->updateItem($data);
+			$this->getController()->result($this->accept, 1);
+		} else {
+			if(is_array($data)) {
+				foreach($data as $item){
+					$this->updateItem($item);
+				}
+				$this->getController()->result($this->accept, count($this->accept));
+			} else {
+				$this->getController()->error("No {$name} item to update!");
+			}
+		}
+	}
+
+	protected function updateItem($data,$return = true ){
+		$id = $data[$name.'Id'];
+		$model = KmaActiveRecord::model(ucfirst($name))->findByPk($id);
 			if($model) {
 				$model->attributes = is_array($data) ? $data : array($data);
 
 				if ($model->save()) {
-					$this->getController()->result(array($model->getItemArray()), 1);
+					//$this->getController()->result(array($model->getItemArray()), 1);
+					$this->addToAcceptList(array($model->getItemArray()));
 				} else {
-					print_r ($model->getErrors());
+					//print_r ($model->getErrors());
 					$this->getController()->error("{$name} item can't be updated!");
 				}
 			} else {
 				$this->getController()->error("No {$name} item to update!");
 			}
-		} else {
-			$this->getController()->error("No {$name} item to update!");
-		}
 	}
+
+	protected $accept = array();
+
+	protected function addToAcceptList($item) {
+		return array_push($this->accept,$item);
+	}
+
 }
